d2104c7bec59136147fdf7d406808b4400c8deb6
prakhar197 <43430805+prakhar197@users.noreply.github.com>
2018-09-20 15:52:19 +0530

    Update Smarty.java

--- Smarty.java
+++ Smarty.java
@@ -2,5 +2,5 @@ public class Smarty(){
 
 public static void main(string[] args0){
 
-System.out.println("Oh ho! I am a smarty robot. My name is chitti 2.0");
+System.out.println("Oh ho! I am a smarty robot. My name is chitti 2.0. Is it a edit?");
 }
